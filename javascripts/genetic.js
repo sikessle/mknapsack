4efe2aa5d65f19eac6f3f7309628fb1758e576fa
sikessle <sikessle@MBA-silver.local>
2014-05-31 17:33:33 +0200

    basic structure of algorithm + fitness evaluation

--- javascripts/genetic.js
+++ javascripts/genetic.js
@@ -1,19 +1,83 @@
 var Genetic = (function () {
     'use strict';
 
+    /*
+        Evaluation Module.
+        Evaluates the solutions.
+        It has domain knowledge.
+    */
+    function EvaluationModule() {
+    }
+
+    // evaluation-is-fitness
+    EvaluationModule.prototype.evaluate = function (solution, problem) {
+        var fitness = 0;
+
+        for (var i = 0; i < solution.length; i++) {
+            if (solution[i] === 1) {
+                fitness += problem.profits[i];
+            }
+        }
+
+        return fitness;
+    };
+
+    /*
+        Population Module.
+        Maintains the population.
+        - initialization
+        - population size
+        - replacement of members by given members
+    */
+    function PopulationModule() {
+
+    }
+
+    /*
+        Reproduction Module.
+        - parent selection
+        - fitness techniques
+        - crossover
+        - mutation
+    */
+    function ReproductionModule() {
+
+    }
+
+    /*
+        controls the three modules and their interaction.
+    */
     function Genetic(params, logger) {
         this.params = $.extend({}, params);
         this.logger = logger;
         this.stopwatch = new Stopwatch();
+        this.problem = {};
+        this.evaluation = new EvaluationModule();
+        this.population = new PopulationModule();
+        this.reproduction = new ReproductionModule();
     }
 
-    Genetic.prototype.solve = function (problems) {
+    /*
+        solves the given problems and stops the time.
+    */
+    Genetic.prototype.solve = function (problem) {
         this.stopwatch.start('total');
 
-        //this.logger.log("solved all problems: {}", problems);
+        this.problem = problem;
+        this.startSolving();
 
         var totalTime = this.stopwatch.stop('total');
         this.logger.log('total time: {} ms', totalTime);
+    };
+
+    // the main solving controller
+    Genetic.prototype.startSolving = function () {
+        for (var gen = 0; gen < this.params.generationsLimit; gen++) {
+            this.logger.log("generation {}:", gen);
+            //var solution = [0, 0, 1, 0, 0, 1];
+            //var fitness = this.evaluation.evaluate(solution, this.problem);
+            //this.logger.log("fitness: {}", fitness);
+        }
     };
 
     return Genetic;
--- javascripts/main.js
+++ javascripts/main.js
@@ -4,18 +4,21 @@ $(document).ready(function () {
     var input, parser, problems, genetic, parserLogger, geneticLogger, geneticParams;
 
     geneticParams = {
-        prop: 'val'
+        generationsLimit: 20,
+        populationSize: 10
     };
 
     parserLogger = new Logger('#parser-log textarea');
     geneticLogger = new Logger('#genetic-log textarea');
 
     parser = new ORParser(parserLogger);
+    // TODO setDataLogger (for data capturing)
     genetic = new Genetic(geneticParams, geneticLogger);
 
     $('#parse').click(function () {
+        $('.parser').insertBefore('.results');
         $('.parser .nav-tabs a[href="#parser-log"]').tab('show');
-
+        $('.results .panel').removeClass('panel-success').addClass('panel-default');
         input = $('#input textarea').val();
         parserLogger.clear();
         problems = parser.parse(input);
@@ -27,7 +30,8 @@ $(document).ready(function () {
         $('.results').insertBefore('.parser');
         $('.results .nav-tabs a[href="#genetic-log"]').tab('show');
         $('.results .panel').removeClass('panel-default').addClass('panel-success');
-        genetic.solve(problems);
+        // TODO currently only solves the first problem in the file
+        genetic.solve(problems[0]);
     });
 
     $('#reset').click(function () {
