edd9c555a83884385d9ebd6dfc9169a9d13459cb
sikessle <sikessle@MBA-silver.local>
2014-06-02 02:04:52 +0200

    default values

--- javascripts/genetic.js
+++ javascripts/genetic.js
@@ -388,14 +388,22 @@ var Genetic = (function () {
 
     /**
      * Mutates with a given probability the solution at one position.
+     * Only accepts good mutation which improve the fitness.
      * @param {Solution} solution the solution to mutate
      */
     ReproductionModule.prototype.mutate = function (solution) {
         var r = Math.random();
+        var oldFitness = this.getFitness(solution);
 
         if (this.isProbable(this.mutateProbability)) {
             var pos = Math.floor(Math.random() * solution.length);
             solution[pos] = 1 - solution[pos];
+            var newFitness = this.getFitness(solution);
+            // bad mutation
+            if (newFitness < oldFitness) {
+                // revert mutation
+                solution[pos] = 1 - solution[pos];
+            }
         }
     };
 
